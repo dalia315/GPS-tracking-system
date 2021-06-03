4cde68d8637749ee1b0e7f5ffd17875b1214f922
dalia315 <85292158+dalia315@users.noreply.github.com>
2021-06-04 00:53:01 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "stdint.h"
 #include "C:/Users/DELL/OneDrive/Desktop/project/tm4c123gh6pm.h"
 void SystemInt(void) {}
-void initialize_portF() { // for Red Led
+void initialize_portF(void) { // for Red Led
 SYSCTL_RCGCGPIO_R |=0X20; //clock Enable for portF
 while((SYSCTL_PRGPIO_R &0x20)==0);//delay
 GPIO_PORTF_LOCK_R= 0x40C4F434B;
