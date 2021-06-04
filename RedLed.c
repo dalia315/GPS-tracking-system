b1888ea50bf66709804fb8efeb2e3a2ed9091cc5
dalia315 <85292158+dalia315@users.noreply.github.com>
2021-06-04 15:27:20 +0200

    Update RedLed.c

--- RedLed.c
+++ RedLed.c
@@ -1,4 +1,4 @@
-void RED_LED (int distance){ //assume that the variable distance contains the total distance calculated
+void RED_LED (float distance){ //assume that the variable distance contains the total distance calculated
 	while ( distance > 100) {
 	  GPIO_PORTF_DATA_R |= 0x02; }
 }
