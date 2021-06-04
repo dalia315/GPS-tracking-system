5b480e7a92daf2f09714608bbf0aa1cac801b167
Rana-medhat <85294372+Rana-medhat@users.noreply.github.com>
2021-06-04 15:09:06 +0200

    Update main.c

--- main.c
+++ main.c
@@ -14,21 +14,21 @@ GPIO_PORTF_DIR_R |= 0x02;
 GPIO_PORTF_DEN_R |= 0x02; 
 GPIO_PORTF_DATA_R &= ~0x02; //Intialize led to be off
 }
-
-void initialize_PortA(void) { // for UART
-SYSCTL_RCGCGPIO_R |= 0x01; //clock enable for portA
-while((SYSCTL_PRGPIO_R & 0x01)==0); //delay
-
-	
-	
-void RED_LED (void){
-	GPIO_PORTF_DATA_R |= 0x02;
+void 7Segment_Display(void){ 
+SYSCTL_RCGCGPIO_R |= 0x03; //clock Enable for portB & portA
+while((SYSCTL_PRGPIO_R &0x03)==0); //delay
+GPIO_PORTB_LOCK_R= 0x4C4F434B;
+GPIO_PORTB_CR_R |=0xFC;
+GPIO_PORTB_AMSEL_R &= ~0xFC;
+GPIO_PORTB_PCTL_R &= ~0xFFFFFF00;
+GPIO_PORTB_AFSEL &= ~0xFC;
+GPIO_PORTB_DIR_R |= 0xFC;
+GPIO_PORTB_DEN_R |= 0xFC;
+GPIO_PORTA_LOCK_R = 0x4C4F434B;
+GPIO_PORTA_CR_R |= 0xC0;
+GPIO_PORTA_AMSEL_R &= ~0xC0;
+GPIO_PORTA_PCTL_R &= ~0xFF000000;
+GPIO_PORTA_AFSEL_R &= ~0xCo;
+GPIO_PORTA_DIR_R |= 0xC0;
+GPIO_PORTA_DEN_R |= 0xC0;	
 }
-	
-		
-int main() {
-  int s = 200 ; //varible contains the updated calculated distance
-  initialize_portF();
-  while ( s > 100) { //condition fo RedLed to turn on
-	RED_LED();}
- }
