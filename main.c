e06fed047a218159a8c9a0dc57a05923344eea32
dalia315 <85292158+dalia315@users.noreply.github.com>
2021-06-04 02:58:59 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "stdint.h"
 #include "C:/Users/DELL/OneDrive/Desktop/project/tm4c123gh6pm.h"
 void SystemInt() {}
+
 void initialize_portF(void) { // for Red Led
 SYSCTL_RCGCGPIO_R |=0X20; //clock Enable for portF
 while((SYSCTL_PRGPIO_R &0x20)==0);//delay
@@ -13,7 +14,21 @@ GPIO_PORTF_DIR_R |= 0x02;
 GPIO_PORTF_DEN_R |= 0x02; 
 GPIO_PORTF_DATA_R &= ~0x02; //Intialize led to be off
 }
+
 void initialize_PortA(void) { // for UART
 SYSCTL_RCGCGPIO_R |= 0x01; //clock enable for portA
 while((SYSCTL_PRGPIO_R & 0x01)==0); //delay
+
 	
+	
+void RED_LED (void){
+	GPIO_PORTF_DATA_R |= 0x02;
+}
+	
+		
+int main() {
+  int s = 200 ; //varible contains the updated calculated distance
+  initialize_portF();
+  while ( s > 100) { //condition fo RedLed to turn on
+	RED_LED();}
+ }
