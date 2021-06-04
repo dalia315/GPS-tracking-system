37baca59636c254910480767eaabadc5df788616
Mohamed-Yasser-fathy <85259196+Mohamed-Yasser-fathy@users.noreply.github.com>
2021-06-04 14:11:07 +0200

    Update Display.h

--- Display.h
+++ Display.h
@@ -1,7 +1,7 @@
-void Varying_Delay(double x)
+void Delay()
 {
     NVIC_ST_CTRL_R = 0;
-    NVIC_ST_RELOAD_R = (int)x*15999999;
+    NVIC_ST_RELOAD_R = 15999;
     NVIC_ST_CTRL_R |= 0x5;
     while( (NVIC_ST_CTRL_R & (1<<16) ) == 0);
     NVIC_ST_CTRL_R = 0;
@@ -13,9 +13,9 @@ int units , tenth , hunderdth;
   tenth = 0x20 | ((num/10)%10);
 	hunderdth = 0x40 |( num/100);
 	GPIO_PORTB_DATA_R = units ;
-	Varying_Delay(0.01);
+	Delay();
 	GPIO_PORTB_DATA_R = tenth;
-	Varying_Delay(0.01);
+	Delay();
 	GPIO_PORTB_DATA_R = hunderdth;
-	Varying_Delay(0.01);
+	Delay();
 }
