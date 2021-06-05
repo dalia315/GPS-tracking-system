523f2b8bf7fa8b62f59df11f2b6b682ddbc02da6
Mohamed Mustafa <85295247+m7astfa@users.noreply.github.com>
2021-06-05 18:37:48 +0200

    Update Display.h

--- Display.h
+++ Display.h
@@ -12,14 +12,14 @@ void display(int num)
 {
 int units , tenth , hunderdth;
 	units = 0x40 | ((num%10)<<2);
-  tenth = 0x80| (((num/10)%10)<<2);
+        tenth = 0x80| (((num/10)%10)<<2);
 	hunderdth = (( num/100))<<2;
 	GPIO_PORTB_DATA_R = units ;
 	Varying_Delay(1);
 	GPIO_PORTB_DATA_R = tenth;
 	Varying_Delay(1);
 	GPIO_PORTB_DATA_R = hunderdth;
-	GPIO_PORTA_DATA_R = 0x01;
+	GPIO_PORTA_DATA_R = 0x04;
 	Varying_Delay(1);
 	GPIO_PORTA_DATA_R = 0;
 }
