9172fe60f995275106cafa2c28607d77610ee57b
dalia315 <85292158+dalia315@users.noreply.github.com>
2021-06-13 17:57:09 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,29 +1,27 @@
 #include "stdint.h"
-#include "LCD.h"
-#include "math.h"
+#include <math.h>
 #include "string.h"
 #include "tm4c123gh6pm.h"
 #include "stdlib.h"
-#define isdigit(c) (c >= '0' && c <= '9')
 
 int main()
 	{
 		PortE_Init();
 		PortB_Init();
-		init_lcd();
-		UART5_init ();
+		Display_on_LCD();
+		UART5_init();
 		initialize_portF();
 		char gomla[]={0};
 		double y1=0;
 		double x1=0;
-		double lat;
-		double lon;
+		double lat=0;
+		double lon=0;
 		char LCD[10];
 		char c;
 		double TD = 0;
 		while(1)
 		{
-			lcd_cmd(lcd_Clear);
+			Config_LCD(0x01);
 			Parsing_GPS( &lat , &lon);
 		
 			double x2 = lat;
@@ -35,18 +33,18 @@ int main()
 			y1=y2;
 			RED_LED(TD);
 					while ( TD > 100 ) {
-						lcd_gotoxy(1,1);
-						lcd_write_string("distance:");
-						lcd_gotoxy(1,2);
+						Position_into_LCD(1,1);
+						write_string_LCD("Total distance:");
+						Position_into_LCD(1,2);
 						sprintf(LCD,"%f",TD);
-						lcd_write_string(LCD);
-						delay_ms(1000);
+						write_string_LCD(LCD);
+						delay(1000);
 					}
-			lcd_gotoxy(1,1);
-			lcd_write_string("distance:");
-			lcd_gotoxy(1,2);
-			sprintf(LCD,"%f",TD);
-			lcd_write_string(LCD);
-			delay_ms(1000);
-		  }
+			Position_into_LCD(1,1);
+						write_string_LCD("Total distance:");
+						Position_into_LCD(1,2);
+						sprintf(LCD,"%f",TD);
+						write_string_LCD(LCD);
+						delay(1000);
 }
+		}
