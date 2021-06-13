749a3f85fe31c744ada448cf8170121022af3f59
dalia315 <85292158+dalia315@users.noreply.github.com>
2021-06-13 02:52:22 +0200

    Update UART_initialization.c

--- UART_initialization.c
+++ UART_initialization.c
@@ -1,15 +1,9 @@
-void UART_Init(void) { //initialize UART1 in PB0 and PB1
-	SYSCTL_RCGCUART_R |= 0x0002;
-	SYSCTL_RCGCGPIO_R |= 0x0002;
-	while ((SYSCTL_PRGPIO_R &0x02)==0) {};//delay
-	UART1_CTL_R &= ~0x0001;
-	UART1_IBRD_R = 104;  // assume bus clock is 16 MHZ and baud rate is 9600 bit/sec
-	UART1_FBRD_R = 11;
-	UART1_LCRH_R = 0x0070;
-	UART1_CTL_R = 0x0301;
-	GPIO_PORTB_AFSEL_R |=0x03;
-	GPIO_PORTB_DEN_R |= 0x03;
-        GPIO_PORTB_PCTL_R = ( GPIO_PORTB_PCTL_R & 0xFFFFFF00) + 0x00000011;
-	GPIO_PORTB_AMSEL_R &= ~0x03;
-	GPIO_PORTB_DIR_R |= 0x02;
+void UART5_init (void){ 
+	SYSCTL_RCGCUART_R |= 0x0020;
+	while((SYSCTL_PRUART_R & 0x0020)==0){};
+	UART5_CTL_R &= ~0x0001;
+	UART5_IBRD_R = 104; 
+	UART5_FBRD_R = 11;
+	UART5_LCRH_R = 0x0070;
+	UART5_CTL_R = 0x0301;
 }
